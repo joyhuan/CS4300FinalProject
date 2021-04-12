5543ee66acafa21cfe5a6d04b9932378a54ba599
Joy Qi <joy_huanqi@outlook.com>
2021-04-11 21:15:28 -0500

    fix selenium issue

--- eg2.py
+++ eg2.py
@@ -1,21 +1,11 @@
 from selenium import webdriver
 from selenium.webdriver.common.action_chains import ActionChains
 from selenium.webdriver.common.keys import Keys
-import time 
 
-browser = webdriver.Safari()
+br = webdriver.Safari()
+br.get('https://www.urbanoutfitters.com/shop/converse-chuck-taylor-all-star-canvas-platform-high-top-sneaker?category=SEARCHRESULTS&color=015&searchparams=q%3Dsneaker&type=REGULAR&quantity=1')
 
-browser.get('https://www.urbanoutfitters.com/shop/converse-chuck-taylor-all-star-canvas-platform-high-top-sneaker?category=SEARCHRESULTS&color=015&searchparams=q%3Dsneaker&type=REGULAR&quantity=1')
-
-# save_me = ActionChains(br).key_down(Keys.CONTROL)\
-#          .key_down('s').key_up(Keys.CONTROL).key_up('s')
-# save_me.perform()
-
-Actions action= new Actions(driver)
-action.contextClick(productLink).sendKeys(Keys.ARROW_DOWN).sendKeys(Keys.ARROW_DOWN).sendKeys(Keys.RETURN).build().perform();
-
-html = browser.page_source
-time.sleep(2)
-fileToWrite = open("py_source.html", "w")
-fileToWrite.write(html)
-browser.close()
+save_me = ActionChains(br).key_down(Keys.CONTROL).key_down('s').key_up(Keys.CONTROL).key_up('s')
+save_me.perform()
+save_me = ActionChains(br).key_down(Keys.ENTER).key_up(Keys.ENTER) 
+save_me.perform()
